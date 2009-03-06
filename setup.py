88820c415b5dcae734a6275ed4347f9d62ecd163
liebster <liebster@quintagroup.com>
2009-03-06 15:01:11 +0000

    Add install information for pypi

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(name='quintagroup.seoptimizer',
       version=read("quintagroup", "seoptimizer", "version.txt").strip(),
       description="Quintagroup Search Engine Optimization Tool",
       long_description=read("docs", "README.txt") + "\n" + \
+                       read("docs", "INSTALL.txt") + "\n" + \
                        read("docs", "HISTORY.txt"),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
