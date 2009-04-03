55a5d165b40e215ff090a24f9140827083a2ec5b
mylan <mylan@quintagroup.com>
2009-04-03 09:56:22 +0000

    Add tests for Meta Tags duplication

--- quintagroup/seoptimizer/tests/testQSEOptimizer.py
+++ quintagroup/seoptimizer/tests/testQSEOptimizer.py
@@ -354,16 +354,75 @@ class TestExposeDCMetaTags(PloneTestCase.FunctionalTestCase):
         self.assert_(m, 'DC meta tags not avaliable when createManager=True')
 
 
-TESTS = [TestInstallation, TestResponse, TestExposeDCMetaTags]
+class TestMetaTagsDuplication(PloneTestCase.FunctionalTestCase):
+
+    def afterSetUp(self):
+        self.qi = self.portal.portal_quickinstaller
+        self.basic_auth = 'portal_manager:secret'
+        uf = self.app.acl_users
+        uf.userFolderAddUser('portal_manager', 'secret', ['Manager'], [])
+        user = uf.getUserById('portal_manager')
+        if not hasattr(user, 'aq_base'):
+            user = user.__of__(uf)
+        newSecurityManager(None, user)
+
+        '''Preparation for functional testing'''
+        self.my_doc = self.portal.invokeFactory('Document', id='my_doc')
+        self.my_doc = self.portal['my_doc']
+        self.my_doc.update(description="Document description")
+
+    def test_GeneratorMeta(self):
+        # Get document without customized canonical url
+        abs_path = "/%s" % self.my_doc.absolute_url(1)
+        regen = re.compile('<meta\s+[^>]*name=\"generator\"[^>]*>', re.S|re.M)
+
+        #import pdb;pdb.set_trace()
+
+        # Before product installation
+        html = self.publish(abs_path, self.basic_auth).getBody()
+        lengen = len(regen.findall(html))
+        self.assert_(lengen==1, "There is %d generator meta tag(s) " \
+           "before seoptimizer installation" % lengen)
+
+#         # After PRODUCT installation
+#         self.qi.installProduct(PRODUCT)
+#         html = self.publish(abs_path, self.basic_auth).getBody()
+#         lengen = len(regen.findall(html))
+#         self.assert_(lengen==1, "There is %d generator meta tag(s) " \
+#            "after seoptimizer installation" % lengen)
+
+    def test_DescriptionMeta(self):
+        # Get document without customized canonical url
+        abs_path = "/%s" % self.my_doc.absolute_url(1)
+        regen = re.compile('<meta\s+[^>]*name=\"description\"[^>]*>', re.S|re.M)
+
+        # Before product installation
+        html = self.publish(abs_path, self.basic_auth).getBody()
+        lendesc = len(regen.findall(html))
+        self.assert_(lendesc==1, "There is %d DESCRIPTION meta tag(s) " \
+           "before seoptimizer installation" % lendesc)
+
+#         # After PRODUCT installation
+#         self.qi.installProduct(PRODUCT)
+#         html = self.publish(abs_path, self.basic_auth).getBody()
+#         lendesc = len(regen.findall(html))
+#         self.assert_(lendesc==1, "There is %d DESCRIPTION meta tag(s) " \
+#            "after seoptimizer installation" % lendesc)
+
+
+TESTS = [TestBeforeInstall,
+         TestInstallation,
+         TestResponse,
+         TestExposeDCMetaTags,
+         TestAdditionalKeywords,
+         TestMetaTagsDuplication,
+        ]
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
     for suite_class in TESTS:
         suite.addTest(makeSuite(suite_class))
-
-    suite.addTest(makeSuite(TestBeforeInstall))
-    suite.addTest(makeSuite(TestAdditionalKeywords))
 
     return suite
 
