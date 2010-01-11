56ccebecf7ad34d70dcd5440a9fe6f591dc31536
liebster <liebster@quintagroup.com>
2010-01-11 18:06:30 +0000

    Added condition install or reinstall package

--- quintagroup/seoptimizer/Extensions/Install.py
+++ quintagroup/seoptimizer/Extensions/Install.py
@@ -2,8 +2,11 @@ from Products.CMFCore.utils import getToolByName
 from quintagroup.seoptimizer.config import PROJECT_NAME
 
 
-def uninstall( portal ):
+def uninstall(portal, reinstall=False):
     setup_tool = getToolByName(portal, 'portal_setup')
     setup_tool.setBaselineContext('profile-%s:uninstall'%PROJECT_NAME)
-    setup_tool.runAllImportStepsFromProfile('profile-%s:uninstall'%PROJECT_NAME)
-    return "Ran all uninstall steps."
+    if reinstall:
+        return "Ran reinstall steps."
+    else:
+        setup_tool.runAllImportStepsFromProfile('profile-%s:uninstall'%PROJECT_NAME)
+        return "Ran all uninstall steps."
