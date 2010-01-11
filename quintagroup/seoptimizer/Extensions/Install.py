7192b397dd122048b2d8369f86665620fd27a48e
liebster <liebster@quintagroup.com>
2010-01-11 18:18:31 +0000

    reverting erroneous commit [2582] (svn merge -r 2582:2572 http://svn.quintagroup.com/products/quintagroup.seoptimizer/tags/2.2.0)

--- quintagroup/seoptimizer/Extensions/Install.py
+++ quintagroup/seoptimizer/Extensions/Install.py
@@ -2,11 +2,8 @@ from Products.CMFCore.utils import getToolByName
 from quintagroup.seoptimizer.config import PROJECT_NAME
 
 
-def uninstall(portal, reinstall=False):
+def uninstall( portal ):
     setup_tool = getToolByName(portal, 'portal_setup')
     setup_tool.setBaselineContext('profile-%s:uninstall'%PROJECT_NAME)
-    if reinstall:
-        return "Ran reinstall steps."
-    else:
-        setup_tool.runAllImportStepsFromProfile('profile-%s:uninstall'%PROJECT_NAME)
-        return "Ran all uninstall steps."
+    setup_tool.runAllImportStepsFromProfile('profile-%s:uninstall'%PROJECT_NAME)
+    return "Ran all uninstall steps."
