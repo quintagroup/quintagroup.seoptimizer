5118c16a143ba7161ffa22a4354c65d60b0e3dc6
mylan <mylan@quintagroup.com>
2010-02-09 19:27:12 +0000

    Add tests for adapters, for correct catalog metadata updation.

--- quintagroup/seoptimizer/tests/testCanonicalURL.py
+++ quintagroup/seoptimizer/tests/testCanonicalURL.py
@@ -2,7 +2,7 @@ import re
 from zope.component import queryAdapter
 from Products.Archetypes.interfaces import IBaseContent
 
-from quintagroup.seoptimizer.interfaces import ICanonicalPath
+from quintagroup.seoptimizer.interfaces import ISEOCanonicalPath
 from base import getToolByName, FunctionalTestCase, newSecurityManager
 from config import *
 
@@ -54,16 +54,24 @@ class TestCanonicalURL(FunctionalTestCase):
            "Wrong CANONICAL URL for document: %s, all must be: %s" % (
            foundcurls, mydoc_url_new))
 
+    def test_seoCanonicalAdapterRegistration(self):
+        seocanonical = queryAdapter(self.mydoc, interface=ISEOCanonicalPath)
+        self.assertTrue(seocanonical is not None,
+            "Not registered ISEOCanonicalPath adapter")
+
     def test_canonicalAdapterRegistration(self):
-        canonical = queryAdapter(self.mydoc, interface=ICanonicalPath)
-        self.assertTrue(canonical is not None,
-            "Not registered 'qseo_canonical' adapter")
+        import quintagroup
+        if hasattr(quintagroup, 'canonicalpath'):
+            from quintagroup.canonicalpath.interfaces import ICanonicalPath
+            canonical = queryAdapter(self.mydoc, interface=ICanonicalPath)
+            self.assertTrue(canonical is not None,
+                "Not registered ICanonicalPath adapter")
 
     def test_canonicalAdapter(self):
         purl = getToolByName(self.portal, 'portal_url')
         mydoc_path_rel = '/'+'/'.join(purl.getRelativeContentPath(self.mydoc))
 
-        canonical = queryAdapter(self.mydoc, ICanonicalPath)
+        canonical = queryAdapter(self.mydoc, ISEOCanonicalPath)
         cpath = canonical.canonical_path()
         self.assertTrue(cpath == mydoc_path_rel,
             "By canonical path adapter got: '%s', must be: '%s'" % (
@@ -80,7 +88,50 @@ class TestCanonicalURL(FunctionalTestCase):
         self.assertTrue(newcpath == mydoc_path_rel_new,
             "By canonical path adapter got: '%s', must be: '%s'" % (
              newcpath, mydoc_path_rel_new))
-       
+
+
+    def addCanonicalPathCatalogColumn(self):
+        from Products.CMFPlone.CatalogTool import registerIndexableAttribute
+
+        def canonical_path(obj, portal, **kwargs):
+            """Return canonical_path property for the object.
+            """
+            cpath = queryAdapter(obj, interface=ISEOCanonicalPath)
+            if cpath:
+                return cpath.canonical_path()
+            return None
+
+        registerIndexableAttribute('canonical_path', canonical_path)
+
+        catalog = getToolByName(self.portal, 'portal_catalog')
+        catalog.addColumn(name='canonical_path')
+
+
+
+    def testCatalogUpdated(self):
+        purl = getToolByName(self.portal, 'portal_url')
+        catalog = getToolByName(self.portal, 'portal_catalog')
+        self.addCanonicalPathCatalogColumn()
+
+        canonical = queryAdapter(self.mydoc, ISEOCanonicalPath)
+        cpath = canonical.canonical_path()
+
+        # get catalog data before update
+        mydoc_catalog_canonical = catalog(id="mydoc")[0].canonical_path
+        self.assertTrue(not mydoc_catalog_canonical)
+
+        # Update canonical url property
+        mydoc_url_new = self.mydoc.absolute_url() + '.new'
+        self.publish(self.mydoc_path + '/@@seo-context-properties?' \
+            'seo_canonical_override=checked&seo_canonical=%s' \
+            '&form.submitted=1' % mydoc_url_new, self.basic_auth)
+
+        newcpath = canonical.canonical_path()
+        mydoc_catalog_canonical = catalog(id="mydoc")[0].canonical_path
+        self.assertTrue(newcpath == mydoc_catalog_canonical,
+            "canonical path get by adapter: '%s' not equals to cataloged one: '%s'" % (
+             newcpath, mydoc_catalog_canonical))
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
