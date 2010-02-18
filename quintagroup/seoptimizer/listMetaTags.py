97f890b11040476b014a4ca507ecfe3634b71230
mylan <mylan@quintagroup.com>
2010-02-18 17:24:55 +0000

    #139: Fix listMetaTags patching for not installed SEOptimizer

--- quintagroup/seoptimizer/listMetaTags.py
+++ quintagroup/seoptimizer/listMetaTags.py
@@ -5,6 +5,10 @@ originalListMetaTags = PloneTool.listMetaTags
 def qsListMetaTags(self, context):
     """ Custom listMetaTags method
     """
+    from quintagroup.seoptimizer.browser.interfaces import IPloneSEOLayer
+    if not IPloneSEOLayer.providedBy(self.REQUEST):
+        return originalListMetaTags(self, context)
+    
     return {}
 
 def qsListMetaTagsOriginal(self, context):
