01d43c6551ee56a4086d447e87dc8b315cb06022
liebster <liebster@quintagroup.com>
2010-02-25 14:54:26 +0000

    reverting last commit with message [2867]

--- quintagroup/seoptimizer/setuphandlers.py
+++ quintagroup/seoptimizer/setuphandlers.py
@@ -5,59 +5,64 @@ from Products.CMFCore.Expression import Expression
 logger = logging.getLogger('quintagroup.seoptimizer')
 FIX_PTYPES_DOMAIN = ['Document', 'File', 'News Item']
 
-def removeSkin(site, layer):
+def migrationActions(site):
+    p_props = getToolByName(site, 'portal_properties')
+    seo_props = getToolByName(p_props, 'seo_properties')
+    ctws = list(seo_props.getProperty('content_types_with_seoproperties', []))
+
+    for ptype in site.portal_types.objectValues():
+        for idx, act in enumerate(ptype.listActions()):
+            if act.id == 'seo_properties':
+                if  ptype.id not in ctws:
+                    ctws.append(ptype.id)
+                ptype.deleteActions([idx])
+                logger.log(logging.INFO, "Moved \"SEO Properties\" action from %s type in portal actions." % ptype.id)
+    seo_props.manage_changeProperties(content_types_with_seoproperties=ctws)
+
+def removeSkin(self, layer):
     """ Remove layers.
     """
-    skins_tool = getToolByName(site, 'portal_skins')
-    for skinName in skins_tool.getSkinSelections():
-        skin_paths = skins_tool.getSkinPath(skinName).split(',') 
-        paths = [l.strip() for l in skin_paths if not (l == layer or l.startswith(layer+'/'))]
-        logger.log(logging.INFO, "Removed layers from %s skin." % skinName)
-        skins_tool.addSkinSelection(skinName, ','.join(paths))
+    skinstool = getToolByName(self, 'portal_skins')
+    for skinName in skinstool.getSkinSelections():
+        original_path = skinstool.getSkinPath(skinName)
+        original_path = [l.strip() for l in original_path.split(',')]
+        new_path= []
+        for l in original_path:
+            if (l == layer) or (l.startswith(layer+'/')):
+                logger.log(logging.INFO, "Removed %s layer from %s skin." % (l, skinName))
+                continue
+            new_path.append(l)
+        skinstool.addSkinSelection(skinName, ','.join(new_path))
 
-def removeActions(site):
+def removeActions(self):
     """ Remove actions.
     """
-    types_tool = getToolByName(site, 'portal_types')
-    for ptype in types_tool.objectValues():
-        idxs = [idx_act[0] for idx_act in enumerate(ptype.listActions()) if idx_act[1].id == 'seo_properties']
-        if idxs:
-            ptype.deleteActions(idxs)
-            logger.log(logging.INFO, "Deleted \"SEO Properties\" action for %s type." % ptype.id)
+    tool = getToolByName(self, 'portal_types')
+    for ptype in tool.objectValues():
+        if ptype.getId() in ['File','Document','News Item']:
+            acts = filter(lambda x: x.id == 'seo_properties', ptype.listActions())
+            action = acts and acts[0] or None
+            if action != None:
+                acts = list(ptype.listActions())
+                ptype.deleteActions([acts.index(a) for a in acts if a.getId()=='seo_properties'])
+                logger.log(logging.INFO, "Deleted \"SEO Properties\" action for %s type." % ptype.id)
 
-def removeConfiglet(site, conf_id):
-    """ Remove configlet.
+def remove_configlets(context, conf_ids):
+    """ Remove configlets.
     """
-    controlpanel_tool = getToolByName(site, 'portal_controlpanel')
-    if controlpanel_tool:
-        controlpanel_tool.unregisterConfiglet(conf_id)
-        logger.log(logging.INFO, "Unregistered \"%s\" configlet." % conf_id)
+    configTool = getToolByName(context, 'portal_controlpanel', None)
+    if configTool:
+        for id in conf_ids:
+            configTool.unregisterConfiglet(id)
+            logger.log(logging.INFO, "Unregistered \"%s\" configlet." % id)
 
 def changeDomain(site):
     """ Fixes old versions bug: Change of content type's domain to 'plone'.
     """
-    types_tool = getToolByName(site, 'portal_types')
-    for ptype in [ptypes for ptypes in types_tool.objectValues() if ptypes.id in FIX_PTYPES_DOMAIN]:
+    for ptype in [ptypes for ptypes in site.portal_types.objectValues() if ptypes.id in FIX_PTYPES_DOMAIN]:
         if ptype.i18n_domain == 'quintagroup.seoptimizer':
             ptype.i18n_domain = 'plone'
             logger.log(logging.INFO, "I18n Domain of the type \'%s\' changed to \'plone\'." % ptype.id)
-
-def migrationActions(site):
-    """ Migration actions from portal_types action to portal_actions.
-    """
-    types_tool = getToolByName(site, 'portal_types')
-    pprops_tool = getToolByName(site, 'portal_properties')
-    seoprops_tool = getToolByName(pprops_tool, 'seo_properties')
-    ctws = list(seoprops_tool.getProperty('content_types_with_seoproperties', []))
-
-    for ptype in types_tool.objectValues():
-        idxs = [idx_act[0] for idx_act in enumerate(ptype.listActions()) if idx_act[1].id == 'seo_properties']
-        if idxs:
-            if ptype.id not in ctws:
-                ctws.append(ptype.id)
-            ptype.deleteActions(idxs)
-            logger.log(logging.INFO, "Moved \"SEO Properties\" action from %s type in portal actions." % ptype.id)
-    seoprops_tool.manage_changeProperties(content_types_with_seoproperties=ctws)
 
 def importVarious(context):
     """ Do customized installation.
@@ -82,4 +87,4 @@ def uninstall(context):
     site = context.getSite()
     removeSkin(site, 'quintagroup.seoptimizer' )
     removeActions(site)
-    removeConfiglet(site, 'quintagroup.seoptimizer')
+    remove_configlets(site, ('quintagroup.seoptimizer',))
