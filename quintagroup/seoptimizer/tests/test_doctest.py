e44c3ea2d826ca4f4ca7f5cfe7995fa434d1ecc9
mylan <mylan@quintagroup.com>
2010-02-26 13:01:31 +0000

    Rename all tests modules to easyer and shorter names

--- quintagroup/seoptimizer/tests/test_doctest.py
+++ quintagroup/seoptimizer/tests/test_doctest.py
@@ -8,22 +8,13 @@ from Testing import ZopeTestCase as ztc
 
 from quintagroup.seoptimizer.tests import base
 
-
-class DocFunctionalTestCase(base.FunctionalTestCase):
-    
-    def auterSetUp(self):
-        qi = self.portal.portal_quickinstaller
-        if qi.isProductInstalled(PROJECT_NAME):
-            qi.uninstallProducts(PROJECT_NAME)
-        
-
 def test_suite():
     return unittest.TestSuite([
 
         # Demonstrate the main content types
         ztc.FunctionalDocFileSuite(
             'browser.txt', package='quintagroup.seoptimizer.tests',
-            test_class=DocFunctionalTestCase,
+            test_class=base.FunctionalTestCase,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
                 doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
 
