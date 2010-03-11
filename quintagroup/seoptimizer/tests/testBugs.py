770150c573f8c8985018635ca303e26046c7d7d4
mylan <mylan@quintagroup.com>
2010-03-11 16:53:40 +0000

    #161: Update, clarify testBugs

--- quintagroup/seoptimizer/tests/testBugs.py
+++ quintagroup/seoptimizer/tests/testBugs.py
@@ -5,25 +5,27 @@ from base import *
 class TestBugs(FunctionalTestCase):
 
     def afterSetUp(self):
-        self.basic_auth = 'portal_manager:secret'
-        uf = self.app.acl_users
-        uf.userFolderAddUser('portal_manager', 'secret', ['Manager'], [])
-        user = uf.getUserById('portal_manager')
-        if not hasattr(user, 'aq_base'):
-            user = user.__of__(uf)
-        newSecurityManager(None, user)
+        self.basic_auth = ':'.join((portal_owner,default_password))
+        self.loginAsPortalOwner()
+        # prepare test document
+        my_doc = self.portal.invokeFactory('Document', id='my_doc')
+        self.my_doc = self.portal['my_doc']
+        self.abs_path = "/%s" % self.my_doc.absolute_url(1)
 
     def test_modification_date(self):
         """ Modification date changing on SEO properties edit """
-        my_doc = self.portal.invokeFactory('Document', id='my_doc')
-        my_doc = self.portal['my_doc']
+        form_data = {'seo_title': 'New Title',
+                     'seo_title_override:int': 1,
+                     'form.submitted:int': 1}
 
-        md_before = my_doc.modification_date
-        abs_path = "/%s" % my_doc.absolute_url(1)
-        form_data = {'seo_title': 'New Title',  'seo_title_override:int': 1, 'form.submitted:int': 1}
-        self.publish(path=abs_path+'/@@seo-context-properties', basic=self.basic_auth, request_method='POST', stdin=StringIO(urllib.urlencode(form_data)))
-        md_after = my_doc.modification_date
+        md_before = self.my_doc.modification_date
+        self.publish(path=self.abs_path+'/@@seo-context-properties',
+                     basic=self.basic_auth, request_method='POST',
+                     stdin=StringIO(urllib.urlencode(form_data)))
+        md_after = self.my_doc.modification_date
+
         self.assertNotEqual(md_before, md_after)
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
