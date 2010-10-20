ba6066f0bc98aac0fce2809b3104fdf97e9da997
mylan <mylan@quintagroup.com>
2010-10-20 13:14:13 +0000

    #233: Check SEO keywords: Added error handling, return reasonable info in case of error

--- quintagroup/seoptimizer/browser/keywords.py
+++ quintagroup/seoptimizer/browser/keywords.py
@@ -33,26 +33,34 @@ class ValidateSEOKeywordsView(BrowserView):
             return ts.utranslate(domain='quintagroup.seoptimizer',
                                  msgid=_(u'Keywords list is empty!'),
                                  context=self.context)
-        # request html page of context object
+        # Get html page internally or with external request
         if isExternal:
             # Not pass timeout option because:
             # 1. its value get from the global default timeout settings by default.
             # 2. timeout option added in python 2.6 (so acceptable only in plone4+)
             try:
-                html = urllib2.urlopen(self.context.absolute_url())
-            except urllib2.URLError:
-                # In case of exceed timeout period
-                # or other URL connection errors.
-                html = unicode(self.context()).encode(enc)
+                try:
+                    resp = urllib2.urlopen(self.context.absolute_url())
+                    html = resp.read()
+                finally:
+                    'resp' in locals().keys() and resp.close()
+            except Exception:
+                # In case of exceed timeout period or other URL connection errors.
+                html = None
         else:
             html = unicode(self.context()).encode(enc)
-        page_text = transforms.convert("html_to_text", html).getData()
-                                 
-        # check every keyword on appearing in body of html page
+
+        # If no html - information about problem with page retrieval should be returned
         result = []
-        for keyword in keywords:
-            keyword_on_page = unicode(len(re.findall(u'\\b%s\\b' % keyword, page_text, re.I|re.U)))
-            result.append(' - '.join((keyword, keyword_on_page)))
+        if html is not None:
+            page_text = transforms.convert("html_to_text", html).getData()
+            # check every keyword on appearing in body of html page
+            for keyword in keywords:
+                keyword_on_page = unicode(len(re.findall(u'\\b%s\\b' % keyword, page_text, re.I|re.U)))
+                result.append(' - '.join((keyword, keyword_on_page)))
+        else:
+            result.append("Problem with page retrieval")
+
         return ts.utranslate(domain='quintagroup.seoptimizer',
                              msgid=_(u'number_keywords',
                                default=u'Number of keywords at page:\n${result}',
