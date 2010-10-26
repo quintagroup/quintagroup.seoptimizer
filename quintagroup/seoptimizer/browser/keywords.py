b17b17af62fc80dd4eda0f01de80089915129071
mylan <mylan@quintagroup.com>
2010-10-26 14:55:15 +0000

    #233: Make code more readable

--- quintagroup/seoptimizer/browser/keywords.py
+++ quintagroup/seoptimizer/browser/keywords.py
@@ -37,14 +37,15 @@ class ValidateSEOKeywordsView(BrowserView):
         error_url = ""
         if isExternal:
             # Not pass timeout option because:
-            # 1. its value get from the global default timeout settings by default.
+            # 1. its value get from the global default timeout settings.
             # 2. timeout option added in python 2.6 (so acceptable only in plone4+)
             try:
                 try:
                     resp = urllib2.urlopen(self.context.absolute_url())
                     html = resp.read()
                 finally:
-                    'resp' in locals().keys() and resp.close()
+                    if 'resp' in locals().keys():
+                        resp.close()
             except (urllib2.URLError, urllib2.HTTPError), e:
                 # In case of exceed timeout period or other URL connection errors.
                 info = sys.exc_info()
@@ -57,15 +58,15 @@ class ValidateSEOKeywordsView(BrowserView):
 
         # If no html - information about problem with page retrieval should be returned
         result = []
-        if html is not None:
+        if html is None:
+            sfx = error_url and ", details at %s." % error_url or "."
+            result.append("Problem with page retrieval" + sfx)
+        else:
             page_text = transforms.convert("html_to_text", html).getData()
             # check every keyword on appearing in body of html page
             for keyword in keywords:
                 keyword_on_page = unicode(len(re.findall(u'\\b%s\\b' % keyword, page_text, re.I|re.U)))
                 result.append(' - '.join((keyword, keyword_on_page)))
-        else:
-            sfx = error_url and ", details at %s." % error_url or "."
-            result.append("Problem with page retrieval" + sfx)
 
         return ts.utranslate(domain='quintagroup.seoptimizer',
                              msgid=_(u'number_keywords',
