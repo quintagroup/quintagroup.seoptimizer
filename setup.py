2c5c2b7894c03bc1a58f54d3be324dd7aeea0e31
mylan <mylan@quintagroup.com>
2010-11-12 16:09:50 +0000

    #235: Updated history, bump version to 4.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of quintagroup.seoptimizer
 import os
 from setuptools import setup, find_packages
 
-version = '4.0.3'
+version = '4.0.4'
 
 setup(name='quintagroup.seoptimizer',
       version=version,
