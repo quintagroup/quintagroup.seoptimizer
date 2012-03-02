9d8634e3215d24f87114564d09694dcd2260ff90
kroman0 <kroman0@quintagroup.com>
2012-03-02 10:17:35 +0000

    Prepare quintagroup.seoptimizer 4.1.3.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of quintagroup.seoptimizer
 import os
 from setuptools import setup, find_packages
 
-version = '4.1.2'
+version = '4.1.3'
 
 setup(name='quintagroup.seoptimizer',
       version=version,
